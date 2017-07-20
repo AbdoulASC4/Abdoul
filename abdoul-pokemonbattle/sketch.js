d7647c01e7ef30c9033d83c17e5f17d948119850
AbdoulASC4 <abdoul.ridwane20@gmail.com>
2017-07-20 12:25:21 -0400

    pokemon battle

---
+++ abdoul-pokemonbattle/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(1200, 800);
+    background(4,94,239);
+}
+
+function draw(){
+    rect(30, 575, 1150, 200); //moves
+    rect(30, 50, 1150, 500, 50); // battel box
+    rect(90, 300, 50, 200); // pokemon 1
+    rect(1075, 90, 50, 200); // pokemon 2
+    rect(90, 275, 50, 20); // pokemon 1 hp
+    rect(1075, 65, 50, 20); // pokemon 2 hp
+    rect(70, 600, 200, 50); // move 1
+    rect(70, 700, 200, 50); // move 2
+    rect(900, 700, 200, 50); // move 3
+    rect(900, 600, 200, 50); // move 4
+    rect(900, 400, 250, 100); // text box
+
+}    
