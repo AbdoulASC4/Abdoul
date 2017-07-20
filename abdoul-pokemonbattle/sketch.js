e92fd56c765031830093def1125a71bd4d495705
AbdoulASC4 <abdoul.ridwane20@gmail.com>
2017-07-20 13:45:32 -0400

    pokemon battle

--- abdoul-pokemonbattle/sketch.js
+++ abdoul-pokemonbattle/sketch.js
@@ -15,5 +15,52 @@ function draw(){
     rect(900, 700, 200, 50); // move 3
     rect(900, 600, 200, 50); // move 4
     rect(900, 400, 250, 100); // text box
-
+    rect(475, 50 , 250, 100); //turn box
 }    
+
+var prompt = require("prompt-sync")();
+var questionOne = prompt("what is your name? ");
+var questionTwo = prompt("what is your Gender? ");
+var response = console.log("Hey "+ questionOne + " Lets Battle!!");
+
+var quickAttack = 10;
+var ironTail = 10;
+var Tackle = 10;
+var hydroCannon = 10;
+
+function pokemon(name, type, hp, attack, turn)
+{
+    this.name = name;
+    this.type = type;
+    this.hp = hp;
+    this.attack = attack;
+    this.turn = turn;   
+}
+var yourPokemon = new pokemon("Pikachu", "electric", 80, [["quickAttack", quickAttack], ["ironTail", ironTail]], true);
+//console.log(yourPokemon);
+var yourPokemon = new pokemon("Ninetale", "fire", 80, [["quickAttack", quickAttack], ["ironTail", ironTail]], true);
+
+var cpuPokemon = new pokemon("Blastoise", "water", 80, [["Tackle", Tackle], ["hydroCannon",hydroCannon]], false);
+//console.log(cpuPokemon);
+
+
+function pokeAttack()
+{
+    if(yourPokemon.turn == true)
+    {
+        cpuPokemon.hp = cpuPokemon.hp - yourPokemon.attack[0][1];
+        yourPokemon.turn = false;
+        cpuPokemon.turn = true;
+        console.log(cpuPokemon.hp);
+        console.log("You attacked the enemy");
+    }
+    else if(cpuPokemon.turn == true)
+    {
+        yourPokemon.hp = yourPokemon.hp - cpuPokemon.attack[0][1];
+        yourPokemon.turn = true;
+        cpuPokemon.turn = false;
+        console.log(yourPokemon.hp);
+        console.log("You've been attacked");
+    }
+ }
+pokeAttack();
